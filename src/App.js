7173c5d9e7ba955b7566c3d6cceea35be2819101
nvineng <naveen211063cse@sdbc.ac.in>
2024-10-13 14:47:05 +0530

    bubble sort program and visualizer is completed

--- src/App.js
+++ src/App.js
@@ -2,40 +2,151 @@ import React, { Component } from "react";
 import './App.css';
 import Bar from "./components/Bar";
 
+// Algorithms
+import BubbleSort from './algorithm/BS';
+
+// Icons
+import Play from '@material-ui/icons/PlayCircleOutlineRounded';
+import Forward from '@material-ui/icons/SkipNextRounded';
+import Backward from '@material-ui/icons/SkipPreviousRounded';
+import RotateLeft from '@material-ui/icons/RotateLeft';
+
 class App extends Component {
   state = {
-    array: [],
-    arraSteps: [],
-    colorKey: [],
-    colorSteps: [],
-    count: 10,
-    delay: 100,
-    algorithum: "",
-    timeouts: [],
+    array: [], // stores the current array of values
+    arraSteps: [], // stores the steps taken during the sorting process
+    colorKey: [], // stores color information for each array element
+    colorSteps: [], // stores the color changes during sorting
+    currentStep: 0, // keeps track of the current step of the sorting process
+    count: 10, // number of elements in the array
+    delay: 10, // delay between each step in milliseconds
+    algorithm: "Bubble Sort", // selected sorting algorithm
+    timeouts: [], // stores timeouts for each step of the animation
   };
+
+  ALGORITHM = {
+    'Bubble Sort': BubbleSort,
+  }
 
   componentDidMount() {
     this.generateRandomArray();
   }
+
+  generateSteps = () => {
+    let array = this.state.array.slice();
+    let steps = this.state.arraSteps.slice();
+    let colorSteps = this.state.colorSteps.slice();
+
+    this.ALGORITHM[this.state.algorithm](array, 0, steps, colorSteps);
+
+    this.setState({
+      arraSteps: steps,
+      colorSteps: colorSteps,
+    });
+  };
+
+  clearTimeouts = () => {
+    this.state.timeouts.forEach((timeout) => clearTimeout(timeout));
+    this.setState({ timeouts: [] });
+  };
+
+  clearColorKey = () => {
+    let blankKey = new Array(this.state.count).fill(0);
+    this.setState({
+      colorKey: blankKey,
+      colorSteps: [blankKey],
+    });
+  };
 
   generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
 
   generateRandomArray = () => {
+    this.clearTimeouts();
+    this.clearColorKey();
     const count = this.state.count;
     const temp = [];
-    // const colorKey = new Array(count).fill('gray'); // Initialize colors for bars
 
     for (let i = 0; i < count; i++) {
       temp.push(this.generateRandomNumber(50, 200));
     }
 
+    this.setState(
+      {
+        array: temp,
+        arraSteps: [temp],
+        currentStep: 0,
+      },
+      () => {
+        this.generateSteps();
+      }
+    );
+  };
+
+  changeArray = (index, value) => {
+    let arr = this.state.array;
+    arr[index] = value;
+    this.setState(
+      {
+        array: arr,
+        arraSteps: [arr],
+        currentStep: 0,
+      },
+      () => {
+        this.generateSteps();
+      }
+    );
+  };
+
+  // Update previousStep method
+  previousStep = () => {
+    let currentStep = this.state.currentStep;
+    if (currentStep === 0) return;
+    currentStep -= 1;
     this.setState({
-      array: temp,
-      arraSteps: [temp],
-      // colorKey: colorKey, // Set colorKey state
+      currentStep: currentStep,
+      array: this.state.arraSteps[currentStep],
+      colorKey: this.state.colorSteps[currentStep]
     });
+  };
+
+  // Update nextStep method
+  nextStep = () => {
+    let currentStep = this.state.currentStep;
+    if (currentStep >= this.state.arraSteps.length - 1) return;
+    currentStep += 1;
+    this.setState({
+      currentStep: currentStep,
+      array: this.state.arraSteps[currentStep],
+      colorKey: this.state.colorSteps[currentStep]
+    });
+  };
+
+  start = () => {
+    let steps = this.state.arraSteps;
+    let colorSteps = this.state.colorSteps;
+
+    this.clearTimeouts();
+
+    let timeouts = [];
+    let i = 0;
+
+    while (i < steps.length - this.state.currentStep) {
+      let timeout = setTimeout(() => {
+        let currentStep = this.state.currentStep;
+        this.setState({
+          array: steps[currentStep],
+          colorKey: colorSteps[currentStep],
+          currentStep: currentStep + 1,
+        });
+      }, this.state.delay * i);
+
+      timeouts.push(timeout);
+      i++;
+    }
+
+    this.setState({ timeouts });
   };
 
   render() {
@@ -45,19 +156,46 @@ class App extends Component {
           key={index}
           index={index}
           length={value}
-          color={0}
+          color={this.state.colorKey[index]}
+          changeArray={this.changeArray}
         />
       );
     });
+
+    let playButton;
+    if (this.state.arraSteps.length === this.state.currentStep) {
+      playButton = (
+        <button className="controller" onClick={this.generateRandomArray}>
+          <RotateLeft />
+        </button>
+      );
+    } else {
+      playButton = (
+        <button className="controller" onClick={this.start}>
+          <Play />
+        </button>
+      );
+    }
+
     return (
       <div className="App">
         <div className="frame">
           <div className="barsDiv container card">
             {bars}
           </div>
-          <div className="control-pannel"></div>
-          <div className="pannel"></div>
         </div>
+        <div className="control-pannel">
+          <div className="control-buttons">
+            <button className="controller" onClick={this.previousStep}>
+              <Backward />
+            </button>
+            {playButton}
+            <button className="controller" onClick={this.nextStep}>
+              <Forward />
+            </button>
+          </div>
+        </div>
+        <div className="pannel"></div>
       </div>
     );
   }
--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,84 +1,114 @@
-import React, { useState } from "react"; // Add useState import
+import React, { useState, useEffect } from "react";
 import "./Bar.css";
 
-function Bar({ index, length, color }) {
+function Bar({ index, length, color, changeArray }) {
     const [len, setLen] = useState(length);
 
-    const barStyle = {
-        height: `${length}px`,
-    };
+    useEffect(() => {
+        setLen(length);
+    }, [length])
 
     const colors = [
-        ['rgba(61,90,241,0.5)', 'rgba(61,90,241,0.2)'],
-        ['rgba(255,48,79,1)', 'rgba(255,48,79,0.5)'],
-        ['rgba(131,232,90,0.5)', 'rgba(131,232,90,0.2)'],
+        ['rgba(61,90,241,0.5)', 'rgba(61,90,241,0.5)'],
+        ['rgba(255,48,79,0.5)', 'rgba(255,48,79,0.5)'],
+        ['rgba(131,232,90,0.5)', 'rgba(131,232,90,0.5)'],
     ];
 
     const inputStyle = {
-        position: 'relative',
-        top: Math.floor(length / 2) - 10,
+        position: "relative",
+        top: Math.floor(length / 2) - 12,
         width: `${length}px`,
-        left: -Math.floor(length / 2) + 10,
+        left: -Math.floor(length / 2) + 13,
         border: "none",
+        background: "none",
     };
 
-    const front_bottom = {
-        transform: `translateY(${200 - length}px) rotateX(-90deg)`, // Correct spelling of 'translateY'
-        backgroundColor: `${colors[color][0]}`, // Correct template literal
-        boxShadow: `5px 5px 50px 5px ${colors[color][0]}`, // Correct template literal
-        transition: '0.3s',
+    const bottom = {
+        transform: `translateY(${200 - length}px) rotateX(-90deg)`,
+        backgroundColor: `${colors[color][0]}`,
+        boxShadow: `5px 5px 50px 5px ${colors[color][0]}`,
+        transition: "0.3s",
     };
 
-    const right_left = {
+    const front_back_right_left = {
         height: `${length}px`,
         transform: `translateY(${200 - length}px)`,
-        backgroundColor: `${colors[color][1]}`, // Correct index access
-        boxShadow: `5px 5px 50px 5px ${colors[color][1]}`, // Correct template literal
-        transition: '0.3s',
+        backgroundColor: `${colors[color][1]}`,
+        boxShadow: `5px 5px 50px 5px ${colors[color][1]}`,
+        transition: "0.3s",
+    };
+
+    const quantity = {
+        position: "relative",
+        top: 225,
     };
 
     const handleChange = (e) => {
         let val = e.target.value;
         if (val === "") {
             setLen(0);
+            changeArray(index, 0);
         } else {
             val = parseInt(val);
             if (val > 200) {
                 setLen(200);
+                changeArray(index, 200);
             } else {
                 setLen(val);
+                changeArray(index, val);
             }
         }
     };
 
+    const increaseLength = (e) => {
+        // if (len < 200) {
+        //     const newLen = len + 10 > 200 ? 200 : len + 10;
+        //     setLen(newLen);
+        //     changeArray(index, newLen);
+        //}
+
+        setLen(len + 1);
+        changeArray(index, len);
+    };
+
+
+    const decreaseLength = (e) => {
+        setLen(len - 1);
+        changeArray(index, len)
+    };
+
     return (
         <>
-            <div className="bar" style={barStyle}>
+            <div className="bar">
                 <div className="side top"></div>
-                <div className="side bottom" style={front_bottom}>
-
+                <div className="side bottom" style={bottom}></div>
+                <div className="side right">
+                    <div className="color-bar right-color-bar" style={front_back_right_left}></div>
                 </div>
-                <div className="side right" >
-                    <div className="color-bar right-color-bar" style={right_left}></div>
+                <div className="side left">
+                    <div className="color-bar left-color-bar" style={front_back_right_left}></div>
                 </div>
-                <div className="side left" style={right_left}>
-                    <div className="color-bar left-color-bar" style={right_left}></div>
-
-                </div>
-                <div className="side front" >
-                    <div className="color-bar front-color-bar" style={front_bottom}> </div>
-                    <input
-                        type="number"
-                        length={length}
-                        style={inputStyle}
-                        value={len}
-                        onChange={handleChange}
-                        className="input"
-                    />
-
+                <div className="side front">
+                    <div className="color-bar front-color-bar" style={front_back_right_left}>
+                        <input
+                            type="number"
+                            style={inputStyle}
+                            value={len}
+                            onChange={handleChange}
+                            className="input"
+                        />
+                    </div>
                 </div>
                 <div className="side back">
-                    <div className="color-back back-color-bar" style={front_bottom}></div>
+                    <div className="color-back back-color-bar" style={front_back_right_left}></div>
+                </div>
+                <div className="quantity-nav">
+                    <div className="quantity-button quantity-up" style={quantity} onClick={increaseLength}>
+                        +
+                    </div>
+                    <div className="quantity-button quantity-down" style={quantity} onClick={decreaseLength}>
+                        -
+                    </div>
                 </div>
             </div>
         </>
